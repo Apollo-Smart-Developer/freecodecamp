86e2a2030910ab3a6d331e6d390a0adf37e918eb
Alexey Krylov <kvins9999@gmail.com>
2026-01-03 14:04:39 +0300

    add variable.js

---
+++ introduction_to_JS/variables.js
@@ -0,0 +1,12 @@
+// Что такое переменные и каковы рекомендации по именованию переменных в JavaScript?
+// пример использования variable let без значенияЮ что выведет undefined
+// let age;
+// console.log(age);
+
+// теперь присваеваем к variable начение
+let age = 30;
+console.log(age);
+
+//переназначение variable
+age = 35;
+console.log(age);
