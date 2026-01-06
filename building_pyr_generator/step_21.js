e7073432478494427ff384bca06bb279c18a32d2
Alexey Krylov <kvins9999@gmail.com>
2026-01-06 17:53:03 +0300

    add step_21.js

---
+++ building_pyr_generator/step_21.js
@@ -0,0 +1,8 @@
+//Step 21
+// Arrays are special in that they are considered mutable. This means you can change the value at an index directly.
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows);
