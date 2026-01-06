23dae3537d1f6ccdabb801e9a96b252e37ac5d7c
Alexey Krylov <kvins9999@gmail.com>
2026-01-06 18:58:37 +0300

    add step_24.js

---
+++ building_pyr_generator/step_24.js
@@ -0,0 +1,15 @@
+//Step 24
+// In the last few steps, you learned all about working with arrays. Take a moment to review what you have learned.
+//
+// Start by declaring a cities variable and initializing it as an array of the strings "London", "New York", and "Mumbai". Then log that variable to the console.
+//
+// After logging, change the last element of cities to the string "Mexico City", then log the cities variable again.
+//
+// When done correctly, you should see this output in the console.
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length -1] = "Mexico City";
+console.log(cities);
