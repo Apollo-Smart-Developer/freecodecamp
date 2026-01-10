6198d622ebf7ad95f1747650746a15d25765ef63
Alexey Krylov <kvins9999@gmail.com>
2026-01-11 00:20:51 +0300

    add step_48.js

---
+++ building_pyr_generator/step_48.js
@@ -0,0 +1,9 @@
+//Step 48
+// In order to use a function, you need to call it. A function call tells your application to run the code from the function wherever you choose to call it. The syntax for a function call is the function name followed by parentheses. For example, this code defines and calls a test function.
+const character = "#";
+const count = 8;
+const rows = [];
+function padRow() {
+
+}
+padRow();
