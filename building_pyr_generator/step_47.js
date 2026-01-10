609bec452d84b2b147739652638a538e06d8291c
Alexey Krylov <kvins9999@gmail.com>
2026-01-11 00:18:03 +0300

    add step_47.js

---
+++ building_pyr_generator/step_47.js
@@ -0,0 +1,8 @@
+// Step 47
+// The logic for formatting this pyramid is likely going to get complicated, which means it's a great time to extract that code into a function.
+//
+// A function is a block of code that can be reused throughout your application. Functions are declared with the following syntax:
+const character = "#";
+const count = 8;
+const rows = [];
+function padRow() {}
